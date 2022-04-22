e7df1cb83282d2c7ef9d3cb5a98677c85ccf0783
Partha <parthabiswas2024@gmail.com>
2022-04-22 13:04:17 +0530

    fetched data from api and displayed in cards, added some styling to cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,71 @@
-import './App.css';
-import React from 'react'
-import { Container, GridJustification, Button, Typography, Card, CardContent, CardActions, CardActionArea} from '@material-ui/core';
+import { Container, Typography, Card, CardContent} from '@material-ui/core';
 import CardMedia from '@material-ui/core/CardMedia';
 import {makeStyles} from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core';
+import React from 'react'
+
 
 const useStyles = makeStyles((theme) =>({
   root: {
-    minWidth: '100%',
-    height: '100vh',     
+    minWidth: '100%',   
     minHeight: '100%',    
-    backgroundColor:theme.palette.grey[200],
-    paddingTop: theme.spacing(5),
+    paddingTop: "22px",
   },
   card:{
-    backgroundColor:"pink"
+    boxShadow: '0 3px 5px 2px #A7E7FE',
+    backgroundImage: 'linear-gradient(45deg, #8FDEEA, #BDEBF0, #EBF7F5)',
+
   },
-  grid:{
-    width:"100vw",
-    marginLeft:"0"
+  typography: {
+    fontFamily: [
+      'Lato',
+      'sans-serif',
+    ].join(','),
+  },
+  div:{
+    paddingTop: "10px",
+  },
+  image:{
+    borderRadius:"3px"
   }
 }));
 
-function App() {
+
+
+export default function App() {
   const classes = useStyles();
+
+  const [users, setUsers] = React.useState([]);
+  const f = async () => {
+    const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    setUsers(json.data);
+  };
+  React.useEffect(() => {
+    f();
+  }, []);
   return (
-      <Container className={classes.root}>
-        <Grid container spacing={3}>
-
-          <Grid item sm={3}>{/* card  */}
-            <Card className={classes.card}>
-
-            <CardActionArea>
-                <CardMedia component="img" image='https://thumbs.dreamstime.com/b/yellow-crash-test-dummy-yellow-crash-test-dummy-car-seat-116968697.jpg0' style={{height:300}} />
-
-              <CardContent>
-                <Typography variant='h4'>Post Title</Typography>
-                <Typography variant='subtitle1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae neque, adipisci soluta</Typography>
-              </CardContent>
-
-              </CardActionArea>
-
-              <CardActions>
-                <Button>Read More</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-      
-        </Grid>
-      </Container>
-
+    <Container className={classes.root}>
+      <Grid container spacing={5}>
+        {users.length &&
+          users.map((user) => {
+            return (
+              <Grid item sm={2}>
+                <Card className={classes.card}>
+                  <CardContent>
+                    <CardMedia component="img" className={classes.image} image={user.avatar}/>
+                      <div className={classes.div}>
+                        <Typography className= {classes.typography} variant='h5'>{user.first_name} {user.last_name}</Typography>
+                        <Typography className= {classes.typography} variant='subtitle1'>ID : {user.id}</Typography>
+                        <Typography className= {classes.typography} variant='subtitle1'>Email ID : {user.email}</Typography>
+                      </div>
+                    </CardContent>
+                </Card>
+              </Grid>
+            );
+          })
+        }
+      </Grid>
+    </Container>
   );
 }
-
-export default App;
